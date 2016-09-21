ece3e558564ea12095bb30af3c2847ab88b3f0cb
jeremydiba <jeremydibattista@gmail.com>
2016-09-20 23:23:29 -0400

    Creating packages and file directories for the project

---
+++ src/fxapp/MainFXApplicaton.java
@@ -0,0 +1,16 @@
+package fxapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by jster on 9/20/2016.
+ */
+public class MainFXApplicaton extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public void start(Stage primaryStage) {
+
+    }
+}
