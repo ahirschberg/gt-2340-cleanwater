0d2e33dbb76d3998a91363b27ddd3f0444382d95
ahirschberg <alexhirschberg97@gmail.com>
2016-09-22 14:49:31 -0400

    Update login screen

--- src/model/User.java
+++ src/model/User.java
@@ -4,4 +4,9 @@ package model;
  * Created by jster on 9/20/2016.
  */
 public class User {
+    private byte[] authToken;
+
+    public User(byte[] authToken) {
+        this.authToken = authToken;
+    }
 }
