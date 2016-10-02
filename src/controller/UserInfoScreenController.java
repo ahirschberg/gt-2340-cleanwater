f187f431213cb3ca6654d4cd13535fcc1a410968
Flayneorange <flayne1996@gmail.com>
2016-10-02 14:09:43 -0400

    Non functional user profile edit screen

---
+++ src/controller/UserInfoScreenController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import fxapp.MainFXApplication;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import model.Token;
+import model.User;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+
+public class UserInfoScreenController {
+    private MainFXApplication main;
+
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField streetField;
+    @FXML
+    private TextField cityField;
+    @FXML
+    private TextField stateField;
+    @FXML
+    private TextField countryField;
+    @FXML
+    private TextField zipField;
+    @FXML
+    private TextField orgField;
+
+    @FXML
+    public void initialize() {
+    }
+    
+    public void registerMainApp(MainFXApplication main) {
+        this.main = main;
+    }
+    
+    @FXML
+    public void onSaveProfile() {
+        
+    }
+
+    @FXML
+    public void onCancel() {
+        main.setLogoutScene();
+    }
+
+}
