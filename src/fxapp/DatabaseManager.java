bcd8c0f8e8b5d3894426f5645c1eb84bc5cabd72
ahirschberg <alexhirschberg97@gmail.com>
2016-10-14 13:50:29 -0400

    Update sql to hold basic user data

--- src/fxapp/DatabaseManager.java
+++ src/fxapp/DatabaseManager.java
@@ -15,29 +15,25 @@ public class DatabaseManager {
         }
         Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
         Statement stat = conn.createStatement();
-        stat.executeUpdate("drop table if exists people;");
-        stat.executeUpdate("create table people (name, occupation);");
+        stat.executeUpdate("drop table if exists users");
+        stat.executeUpdate("create table users (name string, token string UNIQUE);");
         PreparedStatement prep = conn.prepareStatement(
-            "insert into people values (?, ?);");
+            "insert into users values (?, ?);");
 
         prep.setString(1, "Gandhi");
-        prep.setString(2, "politics");
+        prep.setString(2, "abcdefg");
         prep.addBatch();
-        prep.setString(1, "Turing");
-        prep.setString(2, "computers");
-        prep.addBatch();
-        prep.setString(1, "Wittgenstein");
-        prep.setString(2, "smartypants");
+        prep.setString(1, "Alex");
+        prep.setString(2, "defghi");
         prep.addBatch();
 
         conn.setAutoCommit(false);
         prep.executeBatch();
         conn.setAutoCommit(true);
 
-        ResultSet rs = stat.executeQuery("select * from people;");
+        ResultSet rs = stat.executeQuery("select * from users;");
         while (rs.next()) {
-            System.out.println("name = " + rs.getString("name"));
-            System.out.println("job = " + rs.getString("occupation"));
+            System.out.printf("got: id: %s name: %s token: %s\n", rs.getRow(), rs.getString("name"), rs.getString("token"));
         }
         rs.close();
         conn.close();
