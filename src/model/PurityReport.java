8b79a2b4a69b67f77fbd32723c39b779303337dc
jeremydiba <jeremydibattista@gmail.com>
2016-10-25 17:55:43 -0400

    Purity Report Screen can be accessed with proper permission levels, Submit NOT implemented

---
+++ src/model/PurityReport.java
@@ -0,0 +1,42 @@
+package model;
+
+public class PurityReport {
+    private double latitude;
+    private double longitude;
+    private int reportNum;
+    private double virusPPM;
+    private double contaminantPPM;
+    private String condition;
+
+    public PurityReport(double latitude, double longitude, double virusPPM, double contaminantPPM, String condition, int reportNum) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.virusPPM = virusPPM;
+        this.contaminantPPM = contaminantPPM;
+        this.condition = condition;
+        this.reportNum = reportNum;
+    }
+    /**
+     * Returns the location of the report
+     * @return location, as a string
+     */
+    public String getLocation() {
+        return latitude + "," + longitude;
+    }
+    public int getReportNum() { return reportNum; }
+    /**
+     * Gets the latitude, as a double
+     * @return latitude
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Gets the longitude, as a double
+     * @return longitude
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+}
