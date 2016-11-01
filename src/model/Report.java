26005f119e285af13457030c8866cf2ceb3d6990
Flayneorange <flayne1996@gmail.com>
2016-11-01 14:45:48 -0400

    Added automatic date tracking for reports.

--- src/model/Report.java
+++ src/model/Report.java
@@ -1,17 +1,21 @@
 package model;
 
+import java.util.Date;
+
 /**
  * A water quality report.
  */
 public abstract class Report {
     private Location location;
     private int reportNum;
+    private Date creationDatetime;
 
     /**
      * Creates a new report
      * @param reportNum the number of the report
      */
     public Report(int reportNum, Location location) {
+        this.creationDatetime = new Date(); //Sets date to moment of allocation (ms precision)
         this.location = location;
         this.reportNum = reportNum;
     }
@@ -21,7 +25,12 @@ public abstract class Report {
      * @return the number of the report
      */
     public int getReportNum() { return reportNum; }
-
+    
+    public Date getCreationDatetime() { return creationDatetime; }
+    
+    public boolean wasReportCreatedFirst(Report other) {
+        return this.creationDatetime.getTime() < other.creationDatetime.getTime();
+    }
 
     public String toString() {
         return String.format("#%d at %s", getReportNum(), getLocation());
